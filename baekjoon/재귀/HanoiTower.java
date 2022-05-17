40685b073e8df3631a8d10e7b7e5d1ea725c65cf
dongkyuu <gitdaram@gmail.com>
2022-05-17 23:26:40 +0900

    [Update] 2022-05-17

--- baekjoon/재귀/HanoiTower.java
+++ baekjoon/재귀/HanoiTower.java
@@ -1,13 +1,37 @@
 package baekjoon.recursion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HanoiTower {
     
     static int cnt = 0;
 
-    public static void hanoi(int n, int a, int b) {
+    public static void hanoi(int n, int start, int end, int tmp, StringBuilder sb) {
 
-        hanoi(n-1, 1, 2);
-        System.out.println(a + " " + b);
-        hanoi(n-1, 2, 3);
+        if(n == 1) {
+            sb.append(start + " " + end + "\n");
+            return;
+        }
+
+        hanoi(n-1, start, tmp, end, sb);
+        sb.append(start + " " + end + "\n");
+        hanoi(n-1, tmp, end, start, sb);
+        
     }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        hanoi(n, 1, 3, 2, sb);
+        
+        System.out.println(sb.length() / 4);
+        System.out.println(sb);
+
+    }
+    
 }
