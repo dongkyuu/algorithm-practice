ad9a8e5e92f5d51daf6cf592748cd17a327ed1f2
dongkyuu <gitdaram@gmail.com>
2022-05-17 22:38:20 +0900

    [Update] ~ing, 2022-05-17

--- baekjoon/재귀/HanoiTower.java
+++ baekjoon/재귀/HanoiTower.java
@@ -2,4 +2,12 @@ package baekjoon.recursion;
 
 public class HanoiTower {
     
+    static int cnt = 0;
+
+    public static void hanoi(int n, int a, int b) {
+
+        hanoi(n-1, 1, 2);
+        System.out.println(a + " " + b);
+        hanoi(n-1, 2, 3);
+    }
 }
