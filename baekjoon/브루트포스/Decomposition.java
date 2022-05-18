fc83b1449da046fe367013cc5ffc9ae14d4dc47c
dongkyuu <gitdaram@gmail.com>
2022-05-19 00:07:55 +0900

    [Update] 리팩토링, 2022-05-19

--- baekjoon/브루트포스/Decomposition.java
+++ baekjoon/브루트포스/Decomposition.java
@@ -5,6 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Decomposition {
+
+    // 리팩토링
+    public static int solution2(int n) {
+
+        int result = 0;
+
+        for(int i = 1; i < n; i++) {
+
+            int sum = 0;
+            int quo = i;
+
+            while(quo != 0) {
+                sum += quo % 10;
+                quo = quo / 10;
+            }
+
+            if(sum + i == n) {
+                result = i;
+                break;
+            }
+
+        }
+
+        return result;
+
+    }
     
     public static int solution(int n) {
         int result = 0;
@@ -35,7 +61,9 @@ public class Decomposition {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        System.out.println(solution(n));
+        System.out.println(solution2(n));
+
+        br.close();
 
     }
 
