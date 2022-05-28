3dc3cb5d07901be0400ed1527f9992f634c6e21b
dongkyuu <gitdaram@gmail.com>
2022-05-28 22:49:39 +0900

    [Update] 2022-05-28

---
+++ baekjoon/정렬/sortingNumber1.java
@@ -0,0 +1,27 @@
+package baekjoon.sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sortingNumber1 {
+    
+    static int[] arr;
+
+    // 1. 선택 정렬
+    public static void selectionSort(int n) {
+        
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int n = Integer.parseInt(br.readLine());
+
+
+        
+    }
+
+}
