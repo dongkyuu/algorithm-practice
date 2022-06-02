843456274dd60c4aef6140062aa4f54674844d0e
dongkyuu <gitdaram@gmail.com>
2022-06-02 23:26:36 +0900

    [Update] rename 2022-06-02

---
+++ baekjoon/정렬/SortingNumber3.java
@@ -0,0 +1,40 @@
+package baekjoon.sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SortingNumber3 {
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[10001];
+
+        for(int i = 0; i < n; i++) {
+            arr[Integer.parseInt(br.readLine())] += 1;
+        }
+        
+        br.close();
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] == 0) {
+                continue;
+            } else if(arr[i] == 1) {
+                sb.append(i).append('\n');
+            } else {
+                for(int j = 0; j < arr[i]; j++) {
+                    sb.append(i).append('\n');
+                }
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
