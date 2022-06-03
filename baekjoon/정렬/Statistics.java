9b528cca2d0ecbec08a29b4513fbcb1c3a5d1856
dongkyuu <gitdaram@gmail.com>
2022-06-03 17:22:16 +0900

    [Upload] 2022-06-03

---
+++ baekjoon/정렬/Statistics.java
@@ -0,0 +1,17 @@
+package baekjoon.sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Statistics {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
+
+}
